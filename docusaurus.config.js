e1a6567339345878b82a8af84e91244f2564ffca
丁少华 <960423114@qq.com>
2022-06-29 18:03:05 +0800

    feat: 更新文档

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,59 +1,59 @@
 // @ts-check
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '丁少华',
-  url: 'https://dingshaohua.com', // seo
+  title: "丁少华",
+  url: "https://dingshaohua.com", // seo
   // baseUrl: '/',
-  baseUrl: '/dsh-book/build/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // github pages自动部署用到.
-  projectName: 'docusaurus', // github pages自动部署用到.
+  baseUrl: "/dsh-book/build/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "facebook", // github pages自动部署用到.
+  projectName: "docusaurus", // github pages自动部署用到.
   i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh'],
+    defaultLocale: "zh",
+    locales: ["zh"],
   },
   presets: [
     [
-      'classic',
-       // @ts-ignore
+      "classic",
+      // @ts-ignore
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          remarkPlugins: [require('mdx-mermaid')],
-          sidebarPath: require.resolve('./sidebars.js'),
-          path: 'docs/redux',
-          routeBasePath: 'redux',
+          remarkPlugins: [require("mdx-mermaid")],
+          sidebarPath: require.resolve("./sidebars.js"),
+          path: "docs/redux",
+          routeBasePath: "redux",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.scss'),
+          customCss: require.resolve("./src/css/custom.scss"),
         },
       }),
     ],
   ],
 
   plugins: [
-    ['docusaurus-plugin-sass',{}],
+    ["docusaurus-plugin-sass", {}],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'life-module',
-        path: 'docs/life',
-        routeBasePath: 'life',
-        sidebarPath: require.resolve('./sidebars.js')
+        id: "life-module",
+        path: "docs/life",
+        routeBasePath: "life",
+        sidebarPath: require.resolve("./sidebars.js"),
       },
     ],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'fe-teach-module',
-        path: 'docs/fe-teach',
-        routeBasePath: 'fe-teach',
-        sidebarPath: require.resolve('./sidebars.js')
+        id: "fe-teach-module",
+        path: "docs/fe-teach",
+        routeBasePath: "fe-teach",
+        sidebarPath: require.resolve("./sidebars.js"),
       },
     ],
   ],
@@ -62,34 +62,57 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: '丁少华',
+        title: "丁少华",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            docsPluginId:'fe-teach-module',
-            type: 'doc',
-            docId: '第1章_前言',
-            label: '前端学习',
+            docsPluginId: "fe-teach-module",
+            type: "doc",
+            docId: "第1章_前言",
+            label: "前端学习",
           },
           {
-            type: 'doc',
-            docId: '1、初识',
-            label: 'redux',
+            type: "doc",
+            docId: "1、初识",
+            label: "redux",
           },
           {
-            docsPluginId:'life-module',
-            type: 'doc',
-            docId: '认识我',
-            label: '碎碎念',
-          }
+            docsPluginId: "life-module",
+            type: "doc",
+            docId: "认识我",
+            label: "碎碎念",
+          },
         ],
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        defaultLanguage: "javascript",
+        magicComments: [
+          {
+            className: "theme-code-block-highlighted-line",
+            line: "highlight-next-line",
+            block: { start: "highlight-start", end: "highlight-end" },
+          },
+          {
+            className: "code-error",
+            line: "error-next",
+            block: { start: "error-start", end: "error-end" },
+          },
+          {
+            className: "code-sucess",
+            line: "sucess-next",
+            block: { start: "sucess-start", end: "sucess-end" },
+          },
+          {
+            className: "code-warn",
+            line: "warn-next",
+            block: { start: "warn-start", end: "warn-end" },
+          },
+        ],
       },
     }),
 };
