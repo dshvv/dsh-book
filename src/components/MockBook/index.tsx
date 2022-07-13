fe61552f09545b2a0a3355f7ccebc2f0a1e945ca
丁少华 <960423114@qq.com>
2022-07-13 13:00:12 +0800

    fix: 优化

--- src/components/MockBook/index.tsx
+++ src/components/MockBook/index.tsx
@@ -3,24 +3,37 @@ import React from "react";
 import styles from "./index.module.scss";
 import book from "@site/static/img/fe/book.png";
 
-
 const MockBook = (props) => {
   const onClick = (src) => {
     window.open(src);
   };
   return (
     <div className={styles.MockBook}>
-      {props.videos.map((item, index) => (
-        <div className={styles.player} key={index} onClick={()=>{onClick(item.src)}}>
-          <div className={styles.video}>
-            <img src={book} />
+      <div className={styles.removeMargin}>
+        {props.videos.map((item, index) => (
+          <div
+            className={styles.player}
+            key={index}
+            onClick={() => {
+              onClick(item.src);
+            }}
+          >
+            <div className={styles.video}>
+              <img src={book} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.title}>
+                <span className={styles.tipIcon}>📗</span>
+                <span dangerouslySetInnerHTML={{ __html: item.title }} />
+              </div>
+              <div className={styles.desc}>
+                <span className={styles.tipIcon}>📢</span>
+                <span dangerouslySetInnerHTML={{ __html: item.desc }} />
+              </div>
+            </div>
           </div>
-          <div className={styles.txt}>
-            <div className={styles.title}><span className={styles.tipIcon}>📗</span><span dangerouslySetInnerHTML={{__html: item.title}}/></div>
-            <div className={styles.desc}><span className={styles.tipIcon}>📢</span><span dangerouslySetInnerHTML={{__html: item.desc}}/></div>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/MockPlayer/index.tsx
+++ src/components/MockPlayer/index.tsx
@@ -11,17 +11,31 @@ const MockPlayer = (props) => {
   };
   return (
     <div className={styles.MockPlayer}>
-      {props.videos.map((item, index) => (
-        <div className={styles.player} key={index} onClick={()=>{onClick(item.src)}}>
-          <div className={styles.video}>
-            <img src={playCircle} />
+      <div className={styles.removeMargin}>
+        {props.videos.map((item, index) => (
+          <div
+            className={styles.player}
+            key={index}
+            onClick={() => {
+              onClick(item.src);
+            }}
+          >
+            <div className={styles.video}>
+              <img src={playCircle} />
+            </div>
+            <div className={styles.txt}>
+              <div className={styles.title}>
+                <span className={styles.tipIcon}>📺</span>
+                <span dangerouslySetInnerHTML={{ __html: item.title }} />
+              </div>
+              <div className={styles.desc}>
+                <span className={styles.tipIcon}>📢</span>
+                <span dangerouslySetInnerHTML={{ __html: item.desc }} />
+              </div>
+            </div>
           </div>
-          <div className={styles.txt}>
-            <div className={styles.title}><span className={styles.tipIcon}>📺</span><span dangerouslySetInnerHTML={{__html: item.title}}/></div>
-            <div className={styles.desc}><span className={styles.tipIcon}>📢</span><span dangerouslySetInnerHTML={{__html: item.desc}}/></div>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
