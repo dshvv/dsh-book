8035bb38b996f27f4ada3776134cd5966bd4662d
丁少华 <960423114@qq.com>
2022-07-13 12:29:43 +0800

    fix: 优化样式

---
+++ src/components/MockPlayer/index.tsx
@@ -0,0 +1,29 @@
+//@ts-nocheck
+import React from "react";
+import styles from "./index.module.scss";
+import playCircle from "@site/static/img/fe/playCircle.png";
+
+console.log(playCircle);
+
+const MockPlayer = (props) => {
+  const onClick = (src) => {
+    window.open(src);
+  };
+  return (
+    <div className={styles.MockPlayer}>
+      {props.videos.map((item, index) => (
+        <div className={styles.player} key={index} onClick={()=>{onClick(item.src)}}>
+          <div className={styles.video}>
+            <img src={playCircle} />
+          </div>
+          <div className={styles.txt}>
+            <div className={styles.title}><span className={styles.tipIcon}>📺</span><span dangerouslySetInnerHTML={{__html: item.title}}/></div>
+            <div className={styles.desc}><span className={styles.tipIcon}>📢</span><span dangerouslySetInnerHTML={{__html: item.desc}}/></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MockPlayer;
