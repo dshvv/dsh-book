a5eb12343af7a266daf5e8563b466a04875c4020
丁少华 <960423114@qq.com>
2022-07-17 22:45:47 +0800

    feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,51 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 import styles from './index.module.scss';
 import Link from '@docusaurus/Link';
-import { getPlatform } from '../utils'
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import { useHistory } from '@docusaurus/router';
+import { Card } from 'antd';
+import { books } from './helper';
 
 
 
 export default function Home(): JSX.Element {
-  // const start = () => {
-  //    router.push(encodeURI("/guide/第1章_什么是JavaScript"));
-  // };
-  return (
-    <BrowserOnly>
-      {() => {
-        const { isPc } = getPlatform();
-        // const bgVideo = require(`@site/static/img/home/home-bg${isPc?'':'-h5'}.mp4`).default;
-        return <Layout>
-          <main>
-            <div className={styles.HomeMain}>
-              {/* <div className={styles.bg}>
-                <video className={styles.fmBoy} src={bgVideo} loop muted autoPlay />
-              </div> */}
-              <div className={styles.content}>
-                <div className={styles.box}>
+
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar') as HTMLDivElement;
+    navbar.style.display = 'none';
+  }, [])
+
+  const router = useHistory();
+  const onStart = (path) => {
+    router.push(encodeURI(path));
+  };
+  return (<Layout>
+    <main>
+      <div className={styles.HomeMain}>
+
+        {books.map((item, index) => (
+          <div className={styles.book} key={index}>
+            <Card
+              onClick={() => { onStart(item.path) }}
+              style={{ width: 300 }}
+              cover={
+                <img
+                  alt="example"
+                  src={item.logo}
+                />
+              }
+            >
+              <Card.Meta
+                title={item.title}
+                description={item.description}
+              />
+            </Card>
+          </div>
+
+        ))}
+
+        {/* <div className={styles.box}>
                   <div className={styles.img}><img src={require('@site/static/img/home/logo.gif').default} /></div>
                   <div className={styles.btn}>
                     <Link
@@ -32,15 +53,9 @@ export default function Home(): JSX.Element {
                       to="fe/第1章_前言">
                       开始探索
                     </Link>
-                    {/* <button onClick={start}>开始阅读</button> */}
                   </div>
-                </div>
-              </div>
-            </div>
-          </main>
-        </Layout>
-      }}
-
-    </BrowserOnly>
-  );
+                </div> */}
+      </div>
+    </main>
+  </Layout>)
 }
