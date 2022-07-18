3c87df5cf5ee3397016ad128d3a984d05d27fbb6
丁少华 <960423114@qq.com>
2022-07-18 14:49:59 +0800

    feat: 原来pages里只能写页面组件

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,46 @@
 // @ts-nocheck
-import React, { useEffect } from 'react';
-import Layout from '@theme/Layout';
-import styles from './index.module.scss';
-import Link from '@docusaurus/Link';
-import { useHistory } from '@docusaurus/router';
-// import { Card } from 'antd';
-// import { books } from './helper';
-
-
+import React, { useEffect } from "react";
+import Layout from "@theme/Layout";
+import styles from "./index.module.scss";
+import Link from "@docusaurus/Link";
+import { useHistory } from "@docusaurus/router";
+import { Card } from "antd";
+import books from "../utils/books";
 
 export default function Home(): JSX.Element {
-
   useEffect(() => {
-    const navbar = document.querySelector('.navbar') as HTMLDivElement;
-    navbar.style.display = 'none';
-  }, [])
+    const navbar = document.querySelector(".navbar") as HTMLDivElement;
+    navbar.style.display = "none";
+  }, []);
 
   const router = useHistory();
   const onStart = (path) => {
     router.push(encodeURI(path));
   };
-  return (<Layout>
-    <main>
-      <div className={styles.HomeMain}>
-
-        {/* {books.map((item, index) => (
-          <div className={styles.book} key={index}>
-            <Card
-              onClick={() => { onStart(item.path) }}
-              style={{ width: 300 }}
-              cover={
-                <img
-                  alt="example"
-                  src={item.logo}
-                />
-              }
-            >
-              <Card.Meta
-                title={item.title}
-                description={item.description}
-              />
-            </Card>
+  return (
+    <Layout>
+      <main>
+        <div className={styles.HomeMain}>
+          <div className={styles.book}>
+            {books.map((item, index) => (
+              <div className={styles.book} key={index}>
+                <Card
+                  onClick={() => {
+                    onStart(item.path);
+                  }}
+                  style={{ width: 300 }}
+                  cover={<img alt="example" src={item.logo} />}
+                >
+                  <Card.Meta
+                    title={item.title}
+                    description={item.description}
+                  />
+                </Card>
+              </div>
+            ))}
           </div>
-
-        ))} */}
-
-        {/* <div className={styles.box}>
-                  <div className={styles.img}><img src={require('@site/static/img/home/logo.gif').default} /></div>
-                  <div className={styles.btn}>
-                    <Link
-                      className="button button--secondary button--lg"
-                      to="fe/第1章_前言">
-                      开始探索
-                    </Link>
-                  </div>
-                </div> */}
-      </div>
-    </main>
-  </Layout>)
+        </div>
+      </main>
+    </Layout>
+  );
 }
---
+++ src/utils/books.ts
@@ -0,0 +1,8 @@
+export default [
+    {
+        title:'前端学习',
+        description:'小白新手指导',
+        logo: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+        path: '/fe/第1章_前言'
+    }
+]
